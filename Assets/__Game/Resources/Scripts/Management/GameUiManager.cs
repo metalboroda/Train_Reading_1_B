bd36374235360956c415fa129a789d69ce9426b9
dageron93 <vladb93games@gmail.com>
2024-05-26 14:22:39 +0300

    Lose fix

--- Assets/__Game/Resources/Scripts/Management/GameUiManager.cs
+++ Assets/__Game/Resources/Scripts/Management/GameUiManager.cs
@@ -249,7 +249,7 @@ namespace Assets.__Game.Resources.Scripts.Management
           SwitchCanvas(_loseCanvas);
 
           if (_lastLevel == true)
-            _winNextLevelBtn.gameObject.SetActive(false);
+            _loseNextLevelBtn.gameObject.SetActive(false);
           break;
         case GamePauseState:
           _globalCanvas.SetActive(true);
